7138ed776c127d8c974fe933000a569b6ceb6446
terence <terenceshawn18@outlook.com>
2023-12-21 12:41:43 +0300

    Wrote review class code

--- Appfolder/restaurant.py
+++ Appfolder/restaurant.py
@@ -53,3 +53,17 @@ class Restaurant(Base):
             for review in self.reviews
         ]
         return review_strings
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+
+    customer = relationship('Customer', back_populates='reviews')
+    restaurant = relationship('Restaurant', back_populates='reviews')
+
+    def full_review(self):
+        return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.star_rating} stars."
